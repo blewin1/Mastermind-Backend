2e2fb4de3b5ce68009481d0971631ec7b0a3b438
blewin1 <blewin1@gmail.com>
2020-08-18 11:25:47 -0400

    added user authentication and authorization

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,80 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :update, :destroy]
+  before_action :authenticate_token, except: [:login, :create]
+  before_action :authorize_user, except: [:login, :create, :index]
+
+  # POST /login
+  def login
+    user = User.find_by(email: params[:user][:email])
+    if user && user.authenticate(params[:user][:password])
+      token = create_token(user.id, user.email)
+      render json: {status: 200, token: token, user: user}
+    else
+      render json: {status: 401, message: "Unauthorized"}
+    end
+  end
+
+  # GET /users
+  def index
+    @users = User.all
+
+    render json: @users
+  end
+
+  # GET /users/1
+  def show
+    render json: get_current_user
+  end
+
+  # POST /users
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      render json: @user, status: :created, location: @user
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /users/1
+  def update
+    if @user.update(user_params)
+      render json: @user
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /users/1
+  def destroy
+    @user.destroy
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def user_params
+      params.require(:user).permit(:email, :password_digest, :firstname)
+    end
+    ## create appropriate payload for javascript web token
+    def payload(id, email)
+      {
+        exp: (Time.now + 30.minutes).to_i,
+        iat: Time.now.to_i,
+        iss: ENV['JWT_ISSUER'],
+        user: {
+          id: id,
+          email: email
+        }
+      }
+    end
+    # create a javascript webtoken
+    def create_token(id, username)
+      JWT.encode(payload(id, username), ENV['JWT_SECRET'], 'HS256')
+    end
+end
